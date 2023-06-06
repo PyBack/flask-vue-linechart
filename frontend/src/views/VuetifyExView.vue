e4bc5ff2944bbbd0d69d05f3959fff35ea57e442
PyBack <ggtt7@naver.com>
2023-06-07 02:13:22 +0900

    update vuetify components example - vuetify 버튼 예제 - 텍스트만 정상 출력되고 UI 정상적 표현이 안됨

---
+++ frontend/src/views/VuetifyExView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+      <MyComponent /><br>
+      <v-btn>Success</v-btn><br>
+      <v-btn>Error</v-btn><br>
+      <v-btn>Warning</v-btn><br>
+      <v-btn>Info</v-btn>
+    </div>
+  </template>
+
+  <script>
+  import MyComponent from '@/components/VuetifyExample.vue';
+  export default {
+    name: 'VutifyExample',
+    components: {
+        MyComponent
+    }
+  };
+  </script>
+  
+  <style>
+  /* 스타일을 필요에 따라 추가합니다. */
+  </style>
+  
